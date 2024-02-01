fa376ef86aa07cb44787ff423fe0f81f1327b845
LaDiuM1 <wjddhrtjd123@gmail.com>
2024-02-01 23:28:08 +0900

    예외 클래스 이름 수정

--- src/main/java/com/study/blog/exception/RestExceptionHandler.java
+++ src/main/java/com/study/blog/exception/RestExceptionHandler.java
@@ -2,20 +2,18 @@ package com.study.blog.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.sql.SQLIntegrityConstraintViolationException;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
 
 @RestControllerAdvice
-public class RestValidationExceptionHandler {
+public class RestExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
